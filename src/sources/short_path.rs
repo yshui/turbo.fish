be37e8b3ad4b31de8692c38fcfebcbbd9675de9f
Yuxuan Shui <yshuiv7@gmail.com>
2025-08-06 16:41:37 +0100

    sources/short_path: support highlighting a particular segment of cwd
    Rather than always highlighting the last segment. This can be used to
    highlight, for example, the project root directory.

--- src/sources/short_path.rs
+++ src/sources/short_path.rs
@@ -1,7 +1,7 @@
 use futures_util::StreamExt;
 use snafu::ensure;
 use std::{
-    ffi::OsStr,
+    ffi::{OsStr, OsString},
     future::Future,
     os::unix::ffi::OsStrExt,
     path::{Component, Path, PathBuf},
@@ -10,14 +10,71 @@ use std::{
 use serde::{Deserialize, Serialize};
 
 use crate::sources::{GlobalConfig, UpdateSender};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
+struct PathSegment {
+    full: OsString,
+    abbreviated: OsString,
+}
+
 #[derive(Debug)]
 pub struct Source {
     path: PathBuf,
+    highlight_patterns: Vec<glob::Pattern>,
     cfg: Config,
 }
-#[derive(Deserialize, Serialize, Default, Debug, PartialEq, Eq)]
+#[derive(Deserialize, Serialize, Default, Debug, PartialEq, Eq, Clone)]
 pub struct State {
-    short_path: Option<PathBuf>,
+    absolute: bool,
+    short_path: Vec<PathSegment>,
+    highlight: Option<usize>,
+}
+
+impl State {
+    fn new_home() -> Self {
+        Self {
+            absolute: false,
+            short_path: vec![PathSegment {
+                full: "~".into(),
+                abbreviated: "~".into(),
+            }],
+            highlight: None,
+        }
+    }
+    fn new_absolute() -> Self {
+        Self {
+            absolute: true,
+            short_path: vec![],
+            highlight: None,
+        }
+    }
+    /// Create a state from a path quickly without doing I/O.
+    fn new_path(path: &Path) -> Self {
+        let home = std::env::var_os("HOME");
+        let (mut ret, rest) = if let Some(home) = home
+            && let Ok(path) = path.strip_prefix(&home)
+        {
+            (Self::new_home(), path)
+        } else {
+            (Self::new_absolute(), path.strip_prefix("/").unwrap())
+        };
+
+        for c in rest.components() {
+            let c = match c {
+                Component::Prefix(_)
+                | Component::RootDir
+                | Component::CurDir
+                | Component::ParentDir => unreachable!("invalid cwd"),
+                Component::Normal(c) => c,
+            };
+            ret.short_path.push(PathSegment {
+                full: c.to_os_string(),
+                abbreviated: c.to_os_string(),
+            });
+        }
+
+        ret
+    }
 }
 
 fn default_path_bg() -> super::Color {
@@ -35,15 +92,35 @@ fn default_path_fg() -> super::Color {
     })
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone, Copy)]
+fn default_highlight_patterns() -> Vec<String> {
+    [
+        ".git",
+        ".clangd",
+        "compile_commands.json",
+        "requirements.txt",
+        "pyproject.toml",
+        "Cargo.toml",
+    ]
+    .into_iter()
+    .map(ToString::to_string)
+    .collect()
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Config {
     #[serde(default = "default_path_bg")]
     bg: super::Color,
     #[serde(default = "default_path_fg")]
     fg: super::Color,
-    /// Whether the last segment should be abbreviated as well.
+    /// Whether the last component of the path should be abbreviated as well.
     #[serde(default)]
-    abbrev_last_seg: bool,
+    abbreviate_basename: bool,
+    /// The nearest ancestor containing a file matching one of these patterns
+    /// will be highlighted. For example, this can be a root of a git repository.
+    /// Highlighted segment is displayed in bold and not abbreviated.
+    /// Supports globbing.
+    #[serde(default = "default_highlight_patterns")]
+    highlight_patterns: Vec<String>,
 }
 
 impl Default for Config {
@@ -51,7 +128,8 @@ impl Default for Config {
         Self {
             bg: default_path_bg(),
             fg: default_path_fg(),
-            abbrev_last_seg: false,
+            abbreviate_basename: false,
+            highlight_patterns: default_highlight_patterns(),
         }
     }
 }
@@ -67,74 +145,127 @@ enum Error {
     NonExistentChild { parent: PathBuf, child: PathBuf },
 }
 
-async fn shorten_path(base: &Path, rest: &Path, result: &mut PathBuf) -> Result<(), Error> {
-    let mut curr = base.to_path_buf();
-    for c in rest.components() {
-        let c = match c {
-            Component::Prefix(_)
-            | Component::RootDir
-            | Component::CurDir
-            | Component::ParentDir => unreachable!("invalid cwd"),
-            Component::Normal(c) => c,
-        };
+impl Source {
+    async fn shorten_path(
+        &self,
+        base: &Path,
+        rest: &Path,
+        result: &mut State,
+    ) -> Result<(), Error> {
+        let mut curr = base.to_path_buf();
+        for c in rest.components() {
+            let c = match c {
+                Component::Prefix(_)
+                | Component::RootDir
+                | Component::CurDir
+                | Component::ParentDir => unreachable!("invalid cwd"),
+                Component::Normal(c) => c,
+            };
+            let mut dir = async_fs::read_dir(&curr).await?;
+            let mut path_prefix = 1;
+            let mut found_self = false;
+            while let Some(e) = dir.next().await {
+                let e = e?;
+                let fname = e.file_name();
+                if self
+                    .highlight_patterns
+                    .iter()
+                    .any(|p| p.matches_path(Path::new(&fname)))
+                {
+                    log::debug!(
+                        "highlight is {}, because {}",
+                        curr.display(),
+                        fname.display()
+                    );
+                    result.highlight = Some(result.short_path.len() - 1);
+                }
+                if fname == c {
+                    found_self = true;
+                    continue;
+                }
+                log::trace!("{} {}", c.display(), fname.display());
+                let fname = fname.as_bytes();
+                let c = c.as_bytes();
+                if path_prefix <= fname.len()
+                    && path_prefix < c.len()
+                    && c[..path_prefix] == fname[..path_prefix]
+                {
+                    // use path_prefix - 1, so the first byte always matches,
+                    // path_prefix will always be incremented, avoiding a special case for
+                    // fname.len() == path_prefix
+                    for (cch, fnamech) in c[path_prefix - 1..]
+                        .iter()
+                        .zip(fname[path_prefix - 1..].iter())
+                    {
+                        if cch != fnamech {
+                            break;
+                        }
+                        path_prefix += 1;
+                    }
+                }
+            }
+            ensure!(
+                found_self,
+                NonExistentChildSnafu {
+                    parent: curr,
+                    child: c
+                }
+            );
+            // if component is valid utf8, we truncate it to closest char boundary, otherwise
+            // it's shortened to path_prefix bytes.
+            let shortened = if let Ok(c) = str::from_utf8(c.as_bytes()) {
+                c.ceil_char_boundary(path_prefix)
+            } else {
+                path_prefix
+            };
+            result.short_path.push(PathSegment {
+                abbreviated: OsStr::from_bytes(&c.as_bytes()[..shortened]).to_os_string(),
+                full: c.to_os_string(),
+            });
+            curr.push(c);
+        }
         let mut dir = async_fs::read_dir(&curr).await?;
-        let mut path_prefix = 1;
-        let mut found_self = false;
         while let Some(e) = dir.next().await {
             let e = e?;
             let fname = e.file_name();
-            if fname == c {
-                found_self = true;
-                continue;
-            }
-            log::trace!("{} {}", c.display(), fname.display());
-            let fname = fname.as_bytes();
-            let c = c.as_bytes();
-            if path_prefix <= fname.len()
-                && path_prefix < c.len()
-                && c[..path_prefix] == fname[..path_prefix]
+            if self
+                .highlight_patterns
+                .iter()
+                .any(|p| p.matches_path(Path::new(&fname)))
             {
-                // use path_prefix - 1, so the first byte always matches,
-                // path_prefix will always be incremented, avoiding a special case for
-                // fname.len() == path_prefix
-                for (cch, fnamech) in c[path_prefix - 1..]
-                    .iter()
-                    .zip(fname[path_prefix - 1..].iter())
-                {
-                    if cch != fnamech {
-                        break;
-                    }
-                    path_prefix += 1;
-                }
+                log::debug!(
+                    "highlight is {}, because {}",
+                    curr.display(),
+                    fname.display()
+                );
+                result.highlight = Some(result.short_path.len() - 1);
+                break;
             }
         }
-        ensure!(
-            found_self,
-            NonExistentChildSnafu {
-                parent: curr,
-                child: c
-            }
-        );
-        // if component is valid utf8, we truncate it to closest char boundary, otherwise
-        // it's shortened to path_prefix bytes.
-        let shortened = if let Ok(c) = str::from_utf8(c.as_bytes()) {
-            c.ceil_char_boundary(path_prefix)
-        } else {
-            path_prefix
-        };
-        result.push(OsStr::from_bytes(&c.as_bytes()[..shortened]));
-        curr.push(c);
+        Ok(())
     }
-    Ok(())
 }
 
 impl super::Source for Source {
     type State = State;
     type Config = Config;
-    fn new(&cfg: &Config, _global_config: &GlobalConfig, path: &Path) -> Self {
+    fn new(cfg: &Config, _global_config: &GlobalConfig, path: &Path) -> Self {
+        log::debug!("{:?}", cfg.highlight_patterns);
+        let highlight_patterns = cfg
+            .highlight_patterns
+            .iter()
+            .filter_map(|p| match p.parse() {
+                Ok(p) => Some(p),
+                Err(e) => {
+                    log::warn!("Cannot parse pattern {p}: {e}, ignoring");
+                    None
+                }
+            })
+            .collect();
         Self {
-            cfg,
+            cfg: cfg.clone(),
             path: path.to_path_buf(),
+            highlight_patterns,
         }
     }
     fn start(
@@ -145,10 +276,10 @@ impl super::Source for Source {
         let (result_base, base, rest) = if let Some(home) = std::env::var_os("HOME")
             && let Ok(rest) = self.path.strip_prefix(Path::new(&home))
         {
-            (Path::new("~"), PathBuf::from(home), rest)
+            (State::new_home(), PathBuf::from(home), rest)
         } else {
             (
-                Path::new("/"),
+                State::new_absolute(),
                 "/".into(),
                 self.path.strip_prefix("/").unwrap(),
             )
@@ -157,14 +288,11 @@ impl super::Source for Source {
         let mut wait = notify.waiter();
         async move {
             loop {
-                let mut result = result_base.to_path_buf();
-                match shorten_path(&base, &rest, &mut result).await {
+                let mut result = result_base.clone();
+                match self.shorten_path(&base, &rest, &mut result).await {
                     Ok(()) => {
-                        log::info!("shortened path: {}", result.display());
-                        tx.send(Some(State {
-                            short_path: Some(result),
-                        }))
-                        .await;
+                        log::debug!("shortened path: {:?}", result);
+                        tx.send(Some(result)).await;
                         tx.send(None).await;
                     }
                     Err(e) => {
@@ -176,47 +304,75 @@ impl super::Source for Source {
         }
     }
     fn render(&self, path: &Path, state: &State) -> Vec<super::Segment> {
-        let short_path = state.short_path.clone().unwrap_or_else(|| {
-            let home = std::env::var_os("HOME");
-            if let Some(home) = home
-                && let Ok(path) = path.strip_prefix(&home)
-            {
-                std::path::Path::new("~").join(path)
-            } else {
-                path.to_path_buf()
-            }
-        });
-        let style = anstyle::Style::new()
-            .fg_color(Some(self.cfg.fg.dim(0.9).into()))
-            .bg_color(Some(self.cfg.bg.into()))
-            .effects(anstyle::Effects::new());
-        // render the last segment in bold
-        if let Some(short_parent) = short_path.parent()
-            && !short_parent.as_os_str().is_empty()
-        {
-            let last_seg = if self.cfg.abbrev_last_seg {
-                short_path.strip_prefix(short_parent).unwrap()
-            } else {
-                path.strip_prefix(path.parent().unwrap()).unwrap()
-            };
-            vec![
-                super::Segment {
-                    text: format!("{}/", short_parent.display()),
-                    style,
-                    separator: false,
-                },
-                super::Segment {
-                    text: last_seg.to_string_lossy().into(),
-                    style: style.fg_color(Some(self.cfg.fg.into())).bold(),
-                    separator: true,
-                },
-            ]
+        let state = if !state.absolute && state.short_path.is_empty() {
+            // state is not ready yet, use unabbreviated path
+            State::new_path(path)
         } else {
-            vec![super::Segment {
-                text: short_path.to_string_lossy().into(),
-                style,
-                separator: true,
-            }]
+            state.clone()
+        };
+        let mut ret = Vec::new();
+        let highlight = if let Some(h) = state.highlight {
+            h
+        } else {
+            state.short_path.len()
+        };
+
+        let mut before_highlight = String::new();
+        for p in &state.short_path[..highlight] {
+            if state.absolute || !before_highlight.is_empty() {
+                before_highlight.push('/')
+            }
+            before_highlight.push_str(&p.abbreviated.to_string_lossy());
         }
+        if highlight < state.short_path.len() && (state.absolute || !before_highlight.is_empty()) {
+            before_highlight.push('/')
+        }
+        if !before_highlight.is_empty() {
+            ret.push(super::Segment {
+                text: before_highlight,
+                style: anstyle::Style::new()
+                    .fg_color(Some(self.cfg.fg.dim(0.9).into()))
+                    .bg_color(Some(self.cfg.bg.into()))
+                    .effects(anstyle::Effects::new()),
+                separator: false,
+            });
+        }
+        if highlight < state.short_path.len() {
+            ret.push(super::Segment {
+                text: state.short_path[highlight]
+                    .full
+                    .to_string_lossy()
+                    .into_owned(),
+                style: anstyle::Style::new()
+                    .fg_color(Some(self.cfg.fg.into()))
+                    .bg_color(Some(self.cfg.bg.into()))
+                    .bold(),
+                separator: false,
+            })
+        }
+        let mut after_highlight = String::new();
+        for i in highlight + 1..state.short_path.len() {
+            after_highlight.push('/');
+            after_highlight.push_str(
+                &(if i == state.short_path.len() - 1 && !self.cfg.abbreviate_basename {
+                    &state.short_path[i].full
+                } else {
+                    &state.short_path[i].abbreviated
+                })
+                .to_string_lossy(),
+            );
+        }
+        if !after_highlight.is_empty() {
+            ret.push(super::Segment {
+                text: after_highlight,
+                style: anstyle::Style::new()
+                    .fg_color(Some(self.cfg.fg.dim(0.9).into()))
+                    .bg_color(Some(self.cfg.bg.into()))
+                    .effects(anstyle::Effects::new()),
+                separator: false,
+            })
+        }
+        ret.last_mut().unwrap().separator = true;
+        ret
     }
 }
